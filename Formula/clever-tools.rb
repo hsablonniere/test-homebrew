bf0933e77be14c76d625eadd0d27a621fca6d84b
Clement Delafargue <clement@delafargue.name>
2017-08-18 16:23:54 +0200

    clever-tools: 0.9.0 release

--- Formula/clever-tools.rb
+++ Formula/clever-tools.rb
@@ -1,9 +1,9 @@
 class CleverTools < Formula
   desc "Command Line Interface for Clever Cloud"
   homepage "https://github.com/CleverCloud/clever-tools"
-  url "https://clementd-files.cellar.services.clever-cloud.com/clever-tools/macos.tar.gz"
-  version "0.8.3"
-  sha256 "95b8ff6fa0933765d1208f656bccd54cbc97c7cd65097099b42866f7eba14695"
+  url "https://clever-tools.cellar.services.clever-cloud.com/releases/0.9.0/clever-tools-0.9.0_macos.tar.gz"
+  version "0.9.0"
+  sha256 "5c4f995bff01fcdaae599e9ad86b0ab4e96f73ff4975be20bd0896b4c32b2224"
 
   depends_on "bash-completion"
   depends_on "zsh-completion"
