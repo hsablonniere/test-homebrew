3d3e48f6a492c704b9c65e56f047366b3bd27cc4
Clever Cloud CI <ci@clever-cloud.com>
2018-10-30 18:25:24 +0000

    Update to 1.1.0

--- Formula/clever-tools.rb
+++ Formula/clever-tools.rb
@@ -1,9 +1,9 @@
 class CleverTools < Formula
   desc "Command Line Interface for Clever Cloud."
   homepage "https://github.com/CleverCloud/clever-tools"
-  url "https://clever-tools.cellar.services.clever-cloud.com/releases/1.0.2/clever-tools-1.0.2_macos.tar.gz"
-  version "1.0.2"
-  sha256 "5e900ad12094c79ce3e2b96d0f655bb023b47e459554fd7c5a4d90c2dbdac272"
+  url "https://clever-tools.cellar.services.clever-cloud.com/releases/1.1.0/clever-tools-1.1.0_macos.tar.gz"
+  version "1.1.0"
+  sha256 "9f203c69bb957cbd5d6d968156fbf802b26fcf0dcaf5b2e1454ecbf20fc10191"
 
   depends_on "bash-completion" => :recommended
   depends_on "zsh-completions" => :recommended
